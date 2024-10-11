271824d80ff274db177106da1014d1023ba912e9
Jon-nas <jns.lima@outlook.com>
2024-10-11 10:41:31 -0300

    construtor: estado de um objeto

---
+++ src/construtor/Aluno.java
@@ -0,0 +1,30 @@
+package construtor;
+
+//Exemplo utilizando construtor
+import java.util.Random;
+
+public class Aluno {
+    // atributos
+    private String nome;
+    private int idade;
+    private double codigo_identificador;
+    private Random aleatorio;
+
+    // construtor
+    public Aluno(String nome, int idade) {
+        Aluno novoAluno = new Aluno("teste de instanciaão", 50)
+        aleatorio = new Random();
+        this.nome = nome;
+        this.idade = idade;
+        this.codigo_identificador = aleatorio.nextDouble();
+    }
+
+    // métodos
+    public void definirNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void definirIdade(int idade) {
+        this.idade = idade;
+    }
+}
