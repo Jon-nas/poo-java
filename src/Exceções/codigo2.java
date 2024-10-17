d6ff087615475ee4e17b02232053fd4affa3b8a9
Jon-nas <jns.lima@outlook.com>
2024-10-17 15:32:46 -0300

    divisao e bloco try-catch

---
+++ src/Exceções/codigo2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class codigo2 {
+    public class Principal {
+        public static void main ( String args [ ] ) throws InterruptedException {
+            int divisor , dividendo , quociente = 0;
+            String controle = "s";
+	        
+            Scanner s = new Scanner ( System.in );
+            do {    
+                System.out.println ( "Entre com o dividendo." );
+                dividendo = s.nextInt();
+                System.out.println ( "Entre com o divisor." );
+                divisor = s.nextInt();
+                quociente = dividendo / divisor;
+                System.out.println ( "O quociente é: " + quociente );
+                System.out.println ( "Repetir?" );
+                controle = s.next().toString();
+            } while ( controle.equals( "s" ) );
+            s.close();
+        }    
+    }   
+}
