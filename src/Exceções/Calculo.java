986294432add86950c4a02ae6355b269c3f09dc2
Jon-nas <jns.lima@outlook.com>
2024-10-21 15:09:36 -0300

    excecoes de chamadas recursivas

---
+++ src/Exceções/Calculo.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calculo {
+    private static Fibonacci fib;
+    private static Scanner entrada;
+
+    public static void main(String args[]){
+        double num = 0;
+        entrada = new Scanner(System.in);
+        fib = new Fibonacci();
+        do{
+            System.out.println("Entre com um numero não negativo ou \"-1\" para sair: ");
+            num = entrada.nextDouble();
+            if(num == -1)
+                System.exit(0);
+            System.out.println("O " + num + "-esimo termo de Fibonacci eh: " + fib.CalcularFibonacci(num));
+        } while(num >= 0);
+    }
+}
---
+++ src/Exceções/ExemploExcecao.java
@@ -0,0 +1,29 @@
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        System.out.println("Entre com uma senha: ");
+        String senha = "123a";
+
+        try {
+            validarSenha(senha);
+            System.out.println("Senha válida!");
+        } catch (SenhaInvalidaExcecao e) {
+            System.out.println("Senha inválida: " + e.getMessage());
+        }
+    }
+
+    public static void validarSenha(String senha) throws SenhaInvalidaExcecao{
+        if(senha.length() < 5){
+            throw new SenhaInvalidaExcecao("A senha deve ter, pelo menos, 5 caracteres.")
+        }
+
+        else(!senha.matches(".*\\d.*")){
+            throw new SenhaInvalidaExcecao("A senha deve conter, no mínimo, 1 dígito.")
+        }
+    }
+}
+
+class SenhaInvalidaExcecao extends Exception {
+    public SenhaInvalidaExcecao(String menssagem) {
+        super(menssagem);
+    }
+}
